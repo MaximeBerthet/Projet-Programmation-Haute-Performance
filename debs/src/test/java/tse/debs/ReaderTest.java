6d38f4025669a7357c8702000992199abe898add
vicbl <victor.blanchard@telecom-st-etienne.fr>
2017-04-28 13:54:26 +0200

    Modif classe de test Reader

--- debs/src/test/java/tse/debs/ReaderTest.java
+++ debs/src/test/java/tse/debs/ReaderTest.java
@@ -2,6 +2,9 @@ package tse.debs;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
@@ -9,30 +12,36 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 
-public class ReaderTest 
-    extends TestCase
-{
+public class ReaderTest extends TestCase {
 	@Test
 	public void testReader() throws IOException {
 		String path = "D:/Utilisateur/Victor/Bureau/Projet Haute performance/Tests/Q1BigTest/posts.dat";
-		
-		String expected="2010-02-11T19:23:29.459+0000|299394|4661|photo299394.jpg|Michael Wang";
-		Reader inputFile=new Reader(path);
-		
-		ArrayList<String[]> tempList=inputFile.getList();
-		String[] result=tempList.get(7);
-		String finale=Arrays.toString(result).replace(", ", "|").replaceAll("[\\[\\]]", "");
-		
-		
-		// On vérifie que les lignes soient les mêmes
-		assertEquals(expected,finale);
-		
-		// On vérifie que le nombre de champ est bien égal
-		assertEquals(5, result.length);
-		
+
+		Reader inputFile = new Reader(path);
+
+		ArrayList<String[]> tempList = inputFile.getList();
+
+		FileReader file = new FileReader(new File(path));
+		BufferedReader br = new BufferedReader(file);
+		String temp = null;
+		int i = 0;
+		while ((temp = br.readLine()) != null) {
+			String[] result = tempList.get(i);
+			String finale = Arrays.toString(result).replace(", ", "|").replaceAll("[\\[\\]]", "");
+
+			// On vérifie que les lignes soient les mêmes
+			assertEquals(temp, finale);
+
+			// On vérifie que le nombre de champ est bien égal
+			assertEquals(5, result.length);
+
+			i++;
+		}
+		br.close();
+
 		// On vérifie que le fichier est bien un posts
 		assertEquals(1, inputFile.getPostOrComment());
-		
+
 	}
 
 }
