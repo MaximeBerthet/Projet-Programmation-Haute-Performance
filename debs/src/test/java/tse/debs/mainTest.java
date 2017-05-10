b17dd133d8747a9866c12119b5fc829b31d3608c
BaptisteWolff <baptist21@live.fr>
2017-05-10 14:37:09 +0200

    mainTest

--- debs/src/test/java/tse/debs/mainTest.java
+++ debs/src/test/java/tse/debs/mainTest.java
@@ -4,47 +4,78 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import org.joda.time.DateTime;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 public class mainTest {
 
+	static Tri tri = new Tri();
+	static int[] bestScores;
+
+	// static String mainPath = "D:/Utilisateur/Victor/Bureau/Projet Haute
+	static String mainPath = "D:/Users/Baptiste/Documents/Telecom Saint-Etienne/FISE 2/Semestre 8/ProgrammationHautePerformance/Projet/Tests";
+	static String folderName = "Q1PostExpiredComment";
+	static String path = mainPath + "/" + folderName + "/";
+
+	static Reader reader = new Reader(path);
+
+	static Scores scores = new Scores();
+
+	static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+			.withLocale(Locale.ROOT).withChronology(ISOChronology.getInstanceUTC());
+
+	static Display display = new Display(scores, folderName);
+
 	@Test
 	public void test() throws IOException {
-		String path = "D:/Utilisateur/Victor/Bureau/Projet Haute performance/Tests/Q1Case4/posts.dat";
-		String path2 = "D:/Utilisateur/Victor/Bureau/Projet Haute performance/Tests/Q1Case4/comments.dat";
 		
-		Reader r=new Reader(path);
-		Reader r2=new Reader(path2);
+		display.resetLogs();
 		
-		ArrayList<String[]> a = r2.getList();
-	
-	System.out.println("size a "+a.size());
-	String[] line = a.get(0);
-		int i=0;
-		while (i<line.length) {
-			System.out.println("ibis="+i+" : " +line[i]);
-			i++;
+		String[] linePost = reader.readLinePosts();
+		String[] lineComment = reader.readLineComments();
+
+		DateTime datePost = null;
+		DateTime dateComment = null;
+
+		while (linePost[0] != (null) || lineComment[0] != (null)) {
+			if (linePost[0] == (null) || lineComment[0] == (null)) {
+				if (linePost[0] == (null)) {
+					scores.openComment(lineComment);
+					dateComment = formatter.parseDateTime(lineComment[0]);
+					scores.calcul(dateComment);
+					lineComment = reader.readLineComments();
+				} else {
+					scores.openPost(linePost);
+					datePost = formatter.parseDateTime(linePost[0]);
+					scores.calcul(datePost);
+					linePost = reader.readLinePosts();
+				}
+
+			} else {
+				datePost = formatter.parseDateTime(linePost[0]);
+				dateComment = formatter.parseDateTime(lineComment[0]);
+				if (dateComment.isBefore(datePost)) {
+					scores.openComment(lineComment);
+					scores.calcul(dateComment);
+					lineComment = reader.readLineComments();
+				} else {
+					scores.openPost(linePost);
+					scores.calcul(datePost);
+					linePost = reader.readLinePosts();
+
+				}
+			}
+
+			bestScores = tri.Trier(Scores.getPostsScores());
+			display.addLine(bestScores);
 		}
-		
 
-		Scores sc=new Scores();
-		sc.processFile(r.getList(),r.getPost());
-		sc.processFile(r2.getList(),r2.getPost());
-
-		//System.out.println("comment"+Scores.getCommentsIds().get(0));
-		
-	
-		Tri tr= new Tri();
-		
-		int [] fin=tr.Trier(Scores.getPostsScores());
-		
-		//System.out.println("      "+sc.getPostsScores().get(fin[0])+"  "+sc.getPostsScores().get(fin[1])+"  "+sc.getPostsScores().get(fin[2]));
-		Display disp=new Display(Scores.getPostsScores(),Scores.getPostsNbComments(),Scores.getPostsStartDates(), Scores.getPostsAuthors(),Scores.getPostsIds(), fin);
-		
-		
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
